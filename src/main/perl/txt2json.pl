1a4818854e73219c791fc82a8a70730a7c7a42ca
David Shaw <david.shaw@shawware.com.au>
2020-06-06 12:40:40 +1000

    Add a script for converting a succinct text-based format for grid descriptions to our JSON format.

---
+++ src/main/perl/txt2json.pl
@@ -0,0 +1,60 @@
+#! /usr/bin/perl -w
+
+# Format is strictly "op<space>value<space>coord[<space>coord]*
+my(@cages);
+my($size) = 0;
+while (<STDIN>) {
+	chomp;
+	my($op, $val, @coords) = split(/ /);
+
+	my(@squares);
+	foreach (@coords) {
+		s/^\(//;
+		s/\)$//;
+		my($x, $y) = split(/,/);
+		my($square) = {
+			'x' => $x,
+			'y' => $y
+		};
+		push(@squares, $square);
+		$size = $x if $x > $size;
+	}
+
+	my($cage) = {
+		'op' => $op,
+		'val' => $val,
+		'squares' => \@squares
+	};
+
+	push(@cages, $cage);
+}
+
+print("{\n");
+print("\t\"size\": $size,\n");
+print("\t\"cages\": [\n");
+foreach (@cages) {
+	print("\t\t{\n");
+	print("\t\t\t\"operation\": \"$_->{'op'}\",\n");
+	print("\t\t\t\"value\": $_->{'val'},\n");
+	print("\t\t\t\"squares\": [\n");
+	my(@squares) = @{ $_->{'squares'} };
+	foreach (@squares) {
+		my(%s) = %{ $_ };
+		print("\t\t\t\t{\n");
+		print("\t\t\t\t\t\"x\": $s{'x'},\n");
+		print("\t\t\t\t\t\"y\": $s{'y'}\n");
+		print("\t\t\t\t}");
+		if ($_ != $squares[-1]) {
+			print(",");
+		}
+		print("\n");
+	}
+	print("\t\t\t]\n");
+	print("\t\t}");
+	if ($_ != $cages[-1]) {
+		print(",");
+	}
+	print("\n");
+}
+print("\t]\n");
+print("}\n");
